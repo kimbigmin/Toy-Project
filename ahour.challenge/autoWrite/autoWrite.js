d2b6af2a974ef973548d29e55df1598e0aea9f9e
kimbigmin <76110448+kimbigmin@users.noreply.github.com>
2021-08-30 23:29:00 +0900

    JavaScript 1 Hour Challenge

---
+++ ahour.challenge/autoWrite/autoWrite.js
@@ -0,0 +1,18 @@
+const text = '안녕하세요.';
+
+let index = 0;
+
+function writeText() {
+  document.body.innerText = text.slice(0, index);
+
+  index++;
+
+  if (index > text.length) {
+    index = 0;
+  }
+};
+
+
+setInterval(writeText, 100);
+
+
